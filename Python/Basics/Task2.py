7115d5e63ad8c376d3ed09740d3b7cad7a77162c
Harjot-Bakshi <155875843+Harjot-Bakshi@users.noreply.github.com>
2024-08-19 15:03:54 +0530

    Variables & Data types

---
+++ Python/Basics/Task2.py
@@ -0,0 +1,17 @@
+# Variables - To store data or values
+a = 5 #Integer
+b = "Time" #String
+c = 3.25 #Float
+'''Format for Variables are it begins with character and can end with a number - there are no spaces in between and it cannot start with a number. We can use underscore (_) in between characters to create a variable for more readability'''
+
+print(a)
+print(b)
+print(c)
+
+'''type (function) is a way to check the type of a variable'''
+
+print(type(a))
+print(type(b))
+print(type(c))
+
+'''A function is a statement or a way to perform useful operation - it takes argument(s) and work with it. Print() is also a type of function in python, once you print the statments it will give output as <class 'int'> <class 'str'> <class 'float'> for each type a, b & c respectively. In future we will dive into whats a class. Till then try the above out to check'''
