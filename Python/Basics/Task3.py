3907febabd07e57a739da10e89e3e790ebc67223
Harjot-Bakshi <155875843+Harjot-Bakshi@users.noreply.github.com>
2024-08-19 15:40:31 +0530

    Syntax

---
+++ Python/Basics/Task3.py
@@ -0,0 +1,2 @@
+'''Syntax Basics: In Python, functions are fundamental building blocks of coding. A function is a statement that takes arguments and performs operations based on them. The basic syntax of Python requires that functions be called with parentheses () enclosing any arguments passed to them. The same way we saw the function print taking in arguments and giving the output'''
+print("Hello World")
