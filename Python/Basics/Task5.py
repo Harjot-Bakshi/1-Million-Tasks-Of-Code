72e93bd0629a3593cf3c7fbd42a0263e77c64438
Harjot-Bakshi <155875843+Harjot-Bakshi@users.noreply.github.com>
2024-08-19 16:14:08 +0530

    Band name

---
+++ Python/Basics/Task5.py
@@ -0,0 +1,7 @@
+'''Project - Band Name Generator Based on User Input'''
+print("Welcome to Band Name Generator Based on Your Input")
+city = input("Which city are you born in?")
+pet = input("What's your pet name?")
+emotion = input("Choose an emotion?")
+result = city+pet+emotion
+print(f"Your band name is {result}!!!")
