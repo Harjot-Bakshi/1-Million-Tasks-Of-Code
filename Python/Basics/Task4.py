17a44e1b8624539a8d65214cc1fa24cbda63d24a
Harjot-Bakshi <155875843+Harjot-Bakshi@users.noreply.github.com>
2024-08-19 15:58:35 +0530

    Comments & Input

---
+++ Python/Basics/Task4.py
@@ -0,0 +1,6 @@
+'''Comments - Comments are a way to add some instructions in a code so that we can give context to what the code is about. Instead of inputting what the coding is doing like we are adding a values of 5 to the variable d; we generally use comments to input what the code intends to, for example: assigning the distance to the variable d to calculate speed. We use multiple ways to comment (1)Single-Line Comments: Use the # symbol to add comments on a single line. Anything following # on that line is treated as a comment and is ignored by the Python interpreter. (2) Python doesn’t have a specific multi-line comment syntax, but you can use triple quotes """ to create multi-line strings that act as comments.'''
+'''Input is a function used to input from user(s) end'''
+d = 5 # assigning distance to a variable d to calculate speed
+t = 10
+s = d/t
+print(s)
